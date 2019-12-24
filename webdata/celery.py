6e3b64deebeb1207d36696a305805bdd56f90dcf
JerryHsieh <57092545+JerryHsiehNokia@users.noreply.github.com>
2019-12-24 12:16:27 +0800

    update command

--- webdata/celery.py
+++ webdata/celery.py
@@ -8,7 +8,7 @@ from celery.task.schedules import crontab
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webdata.settings') 
 app = Celery('webdata', broker='redis://localhost:6379/')
 
-@periodic_task(run_every=timedelta(minutes=30))
+@periodic_task(run_every=timedelta(minutes=1))
 def some_task():
 	from Spider.spiders.NBA import NBASpider
 	from tools.scrapy_run import run_scrapy
